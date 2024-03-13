f31444062b3fc406bb20d15436000d44202f32dd
Abhishek suman <abhisheksumancode@gmail.com>
2024-03-14 01:29:54 +0530

    socket connected 1.1v

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import { drawLine } from './../socketscribbl/utils/drawLine';
 declare var require: any
 
 const express = require('express')
@@ -5,5 +6,42 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 
-import {server} from 
+import { Server } from 'socket.io'
 
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+})
+
+type Point = { x: Number, y: number }
+
+type DrawLine = {
+    prevPoint: Point | null
+    currentPoint: Point
+    color: string
+}
+
+io.on('connection', (socket) => {
+
+
+    socket.on('client-ready',()=>{
+        socket.broadcast.emit('get-canvas-state')
+    })
+
+    socket.on('canvas-state',(state)=>{
+        socket.broadcast.emit('canvas-state-from-server',state)
+    })
+
+    socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLine) => {
+        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
+    })
+
+    socket.on('clear',() => io.emit('clear'))
+
+
+})
+
+server.listen(3001,() =>{
+    console.log('Listening 3001')
+})
